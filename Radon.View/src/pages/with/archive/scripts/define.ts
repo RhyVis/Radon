6455452132846e789c745b8ce9e2f87ba1772ef0
Rhynia <vssr@foxmail.com>
2024-12-25 21:18:17 +0800

    forgot to commit this

---
+++ Radon.View/src/pages/with/archive/scripts/define.ts
@@ -0,0 +1,20 @@
+﻿export type MdIndexDto = {
+  path: string;
+  name: string;
+  desc: string;
+  createTime: string;
+  updateTime: string;
+};
+
+export type MdIndex = MdIndexDto & { content: string };
+
+export type MdRecord = {
+  name: string;
+  desc: string;
+  content: string;
+};
+
+export type MdUploadImageCallback = (
+  files: Array<File>,
+  callback: (urls: string[] | { url: string; alt: string; title: string }[]) => void,
+) => void;
